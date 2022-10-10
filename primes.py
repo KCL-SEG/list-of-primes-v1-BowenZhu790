a9533976e3450587129caddcd713c02bbd803acb
Bowen Zhu <bowen.zhu@kcl.ac.uk>
2022-10-10 15:59:01 +0100

    Frist try

--- primes.py
+++ primes.py
@@ -1,6 +1,12 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+import numpy as np
 def primes(number_of_primes):
+    i = 1
     list = []
+    while np.size(list) < number_of_primes:
+        k = 1;
+        if k != 1 and k != i and i % k == 0:
+            list.append(i)
+        i++
     return list
